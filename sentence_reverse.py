62321614f97557163aaf5f14039f34913b80b6a1
0Edwin0 <ed549win@gmail.com>
2020-01-09 12:57:20 +0530

    changed strReverse()

--- sentence_reverse.py
+++ sentence_reverse.py
@@ -3,7 +3,7 @@
 # Reverse the whole string without reversing
 # the individual words in it.
 
-def strReverse(string, sep='.'):
+def strReverse(string, sep=None):
     '''
     Return reversed string without reversing 
     individual words in it.
@@ -11,25 +11,32 @@ def strReverse(string, sep='.'):
     string : (str) value for reversing.
 
     sep : (str) The delimiter according which to
-    split the string into words. `.` (the default value) 
-    means split according to any dot.
+    split the string into words. `None` (the default value) 
+    means split according to any whitespaces.
 
     '''
     
     list_str = string.split(sep)
-    rev_list = list_str[::-1]
+    
+    if len(list_str)>2:
+        print(len(list_str),list_str)
+        rev_list = list_str[::-1]
+    else:
+        rev_list = list_str
+    
     if sep:
         v = sep
     else:
         v = ' '
     new_string = v.join(rev_list)
+    
     return new_string
 
 if __name__ == '__main__':
     print("Reverse the whole string without reversing the individual words in it.\n")
     try:
         string = input('Enter String : ')
-        new_str = strReverse(string)
+        new_str = strReverse(string,sep='.')
         print('Reversed String:', new_str)
     except Exception as e :
         print('Error occured : {}'.format(e))
